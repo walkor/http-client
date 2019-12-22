de6254a2a0410b7eeced990bcdf827462c71ef55
walkor <walkor@users.noreply.github.com>
2019-12-22 22:25:18 +0800

    fix mem leak

--- src/ConnectionPool.php
+++ src/ConnectionPool.php
@@ -54,8 +54,7 @@ class ConnectionPool extends Emitter
      */
     public function __construct($option = [])
     {
-        $this->_option = $this->_option = array_merge($this->_option, $option);
-        Timer::add(1, [$this, 'closeTimeoutConnection']);
+        $this->_option = array_merge($this->_option, $option);
     }
 
     /**
@@ -83,6 +82,7 @@ class ConnectionPool extends Emitter
         }
         $this->_using[$address][$connection->id] = $connection;
         $connection->pool['request_time'] = time();
+        $this->tryToCreateConnectionCheckTimer();
         return $connection;
     }
 
@@ -104,8 +104,8 @@ class ConnectionPool extends Emitter
             $connection->pool['idle_time'] = time();
             $connection->onConnect = $connection->onMessage = $connection->onError =
             $connection->onClose = $connection->onBufferFull = $connection->onBufferDrain = null;
-            $connection->onClose = [$this, 'delete'];
         }
+        $this->tryToCreateConnectionCheckTimer();
         $this->emit('idle', $address);
     }
 
@@ -122,6 +122,10 @@ class ConnectionPool extends Emitter
         if (empty($this->_idle[$address])) {
             unset($this->_idle[$address]);
         }
+        unset($this->_using[$address][$connection_id]);
+        if (empty($this->_using[$address])) {
+            unset($this->_using[$address]);
+        }
     }
 
     /**
@@ -129,11 +133,21 @@ class ConnectionPool extends Emitter
      */
     public function closeTimeoutConnection()
     {
+        if (empty($this->_idle) && empty($this->_using)) {
+            Timer::del($this->_timer);
+            $this->_timer = 0;
+            return;
+        }
         $time = time();
         $keepalive_timeout = $this->_option['keepalive_timeout'];
         foreach ($this->_idle as $address => $connections) {
+            if (empty($connections)) {
+                unset($this->_idle[$address]);
+                continue;
+            }
             foreach ($connections as $connection) {
                 if ($time - $connection->pool['idle_time'] >= $keepalive_timeout) {
+                    $this->delete($connection);
                     $connection->close();
                 }
             }
@@ -142,6 +156,10 @@ class ConnectionPool extends Emitter
         $connect_timeout = $this->_option['connect_timeout'];
         $timeout = $this->_option['timeout'];
         foreach ($this->_using as $address => $connections) {
+            if (empty($connections)) {
+                unset($this->_using[$address]);
+                continue;
+            }
             foreach ($connections as $connection) {
                 $state = $connection->getStatus(false);
                 if ($state === 'CONNECTING') {
@@ -150,6 +168,7 @@ class ConnectionPool extends Emitter
                         if ($connection->onError) {
                             call_user_func($connection->onError, $connection, WORKERMAN_CONNECT_FAIL, 'connect ' . $connection->getRemoteAddress() . ' timeout after ' . $diff . ' seconds');
                         }
+                        $this->delete($connection);
                         $connection->close();
                     }
                 } elseif ($state === 'ESTABLISHED') {
@@ -158,6 +177,7 @@ class ConnectionPool extends Emitter
                         if ($connection->onError) {
                             call_user_func($connection->onError, $connection, 128, 'read ' . $connection->getRemoteAddress() . ' timeout after ' . $diff . ' seconds');
                         }
+                        $this->delete($connection);
                         $connection->close();
                     }
                 }
@@ -182,5 +202,15 @@ class ConnectionPool extends Emitter
         $connection->connect();
         $connection->pool = ['connect_time' => time()];
         return $connection;
+    }
+
+    /**
+     * Create Timer.
+     */
+    protected function tryToCreateConnectionCheckTimer()
+    {
+        if (!$this->_timer) {
+            $this->_timer = Timer::add(1, [$this, 'closeTimeoutConnection']);
+        }
     }
 }
