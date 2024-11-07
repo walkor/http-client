f2bd65fc2efc6fd4d7b0dee5c05149656cc73ded
乐于是 <linshengli.linux7@icloud.com>
2024-11-07 17:47:12 +0800

    Remove duplicate code

--- src/Client.php
+++ src/Client.php
@@ -156,10 +156,6 @@ class Client
         $address = $task['address'];
 
         $connection = $this->_connectionPool->fetch($address, strpos($url, 'https') === 0, $task['options']['proxy'] ?? '');
-        $connection->errorHandler = function(Throwable $exception) use ($task) {
-            $this->deferError($task['options'], $exception);
-        };
-
         // No connection is in idle state then wait.
         if (!$connection) {
             return;
